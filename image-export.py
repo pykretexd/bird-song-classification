027d2a3379f41807287ecaaf93bab02afddae285
Sixten <landysixten@gmail.com>
2022-03-09 10:13:02 +0100

    Add dataset and rewrite image-export

--- image-export.py
+++ image-export.py
@@ -5,24 +5,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-df = pd.read_csv('train/train_tp.csv')
+filename = str('test.mp3')
+y, sr = librosa.load('train/' + str(filename))
 
-i = 1
-while i < 5:
-    sample_num = i
-    filename = df.recording_id[sample_num] + str('.flac')
-    tstart = df.t_min[sample_num] 
-    tend = df.t_max[sample_num]
-    y, sr = librosa.load('train/' + str(filename))
+# Mel spectrogram
+s = librosa.feature.melspectrogram(y, sr)
+mel_spectrogram = librosa.power_to_db(s, ref=np.max)
+mel_img = librosa.display.specshow(mel_spectrogram)
+plt.tight_layout()
+plt.savefig("images/{}".format(filename, dpi=100))
 
-    # Mel spectrogram
-    s = librosa.feature.melspectrogram(y, sr)
-    mel_spectrogram = librosa.power_to_db(s, ref=np.max)
-    mel_img = librosa.display.specshow(mel_spectrogram)
-    plt.tight_layout()
-    plt.savefig("images/{}".format(df.recording_id[sample_num], dpi=100))
-
-    print("Image exported.")
-    i += 1
-
-    del y, sr, s, mel_spectrogram, mel_img
+print("Image exported.")
