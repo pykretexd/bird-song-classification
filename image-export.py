bb5d47becc04315e03fb51ed369d3b6afa9c1f9c
Sixten <landysixten@gmail.com>
2022-03-09 10:40:17 +0100

    .gitignore train folder

--- image-export.py
+++ image-export.py
@@ -5,14 +5,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
-filename = str('test.mp3')
-y, sr = librosa.load('train/' + str(filename))
+df = pd.read_csv("train/train_tp.csv")
 
-# Mel spectrogram
-s = librosa.feature.melspectrogram(y, sr)
-mel_spectrogram = librosa.power_to_db(s, ref=np.max)
-mel_img = librosa.display.specshow(mel_spectrogram)
-plt.tight_layout()
-plt.savefig("images/{}".format(filename, dpi=100))
+i = 1
+while i < 479:
+    sample_num = i
+    filename = df.recording_id[sample_num] + str('.flac')
+    tstart = df.t_min[sample_num] 
+    tend = df.t_max[sample_num]
+    y, sr = librosa.load('train/' + str(filename))
 
-print("Image exported.")
+    # Mel spectrogram
+    s = librosa.feature.melspectrogram(y, sr)
+    mel_spectrogram = librosa.power_to_db(s, ref=np.max)
+    mel_img = librosa.display.specshow(mel_spectrogram)
+    plt.tight_layout()
+    plt.savefig("images/{}".format(df.recording_id[sample_num], dpi=100))
+
+    print("Image exported.")
+    i += 1
+
+    del y, sr, s, mel_spectrogram, mel_img
