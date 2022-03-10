1e3025803dcf3416a9385c4cfca7816172890f18
Sixten <landysixten@gmail.com>
2022-03-10 15:32:45 +0100

    audio-to-mel update
    audio_file raises KeyError

--- images/audio-to-mel.py
+++ images/audio-to-mel.py
@@ -7,25 +7,28 @@ from scipy.io.wavfile import read, write
 import matplotlib.pyplot as plt
 
 wav_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'audio', 'wav'))
+df = pd.read_csv(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'audio', 'metadata.csv')))
 
-csv = pd.read_csv(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'audio', 'metadata.csv')))
-
-i = 0
-for filename in os.listdir(wav_path):
-    audio_file = str(csv.Genus[i]) + str("-") + str(csv.Specific_epithet[i]) + str("-") + str(csv.Recording_ID[i]) + str('.wav')
-    output = "images/mel/{0}/{1}.png".format(csv.English_name[i], csv.Recording_ID[i])
+for i in os.listdir(wav_path):
+    audio_file = '{0}-{1}-{2}.wav'.format(df.Genus[i], df.Specific_epithet[i], df.Recording_ID[i])
+    output = "images/mel/{0}/{1}.png".format(df.English_name[i], df.Recording_ID[i])
 
     try:
         y, sr = librosa.load(wav_path + "\\" + audio_file)
+        if os.path.exists(output):
+            print("{} already exists. Continuing.".format(output))
+            continue
+        else:
+            pass
+
     except FileNotFoundError:
-        print(wav_path + "\\" + audio_file + " not found. Continuing.")
-        i += 1
+        print("{} not found. Continuing.".format(output))
         continue
 
     mel_s = librosa.feature.melspectrogram(y, sr)
     mel_spectrogram = librosa.power_to_db(mel_s, ref=np.max)
-
     mel_img = librosa.display.specshow(mel_spectrogram)
+
     plt.tight_layout()
     try:
         plt.savefig(output)
@@ -35,5 +38,5 @@ for filename in os.listdir(wav_path):
         plt.savefig(output)
 
     print("Conversion of {} successful.".format(output))
+
     del y, sr, mel_s, mel_spectrogram, mel_img
-    i += 1
