1e9a4a27981ae16e3966a010e3bb4c76d8c7e031
Sixten <landysixten@gmail.com>
2022-03-11 13:01:07 +0100

    Improve user interface and runtime

--- audio/ogg-convert-trim.py
+++ audio/ogg-convert-trim.py
@@ -1,6 +1,7 @@
 from pydub import AudioSegment
 import os
 import pandas as pd
+from tqdm import tqdm
 
 mp3_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'mp3'))
 ogg_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'ogg'))
@@ -8,13 +9,12 @@ ogg_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'ogg'))
 start_time = 0 # 0 sec
 end_time = 30000 # 30 sec
 
-for filename in os.listdir(mp3_path):
-    name, ext = os.path.splitext(filename)
-    if ext == ".mp3":
-       mp3 = AudioSegment.from_mp3(mp3_path + "\\" + filename)
-       mp3[start_time:end_time].export("{0}/{1}.ogg".format(ogg_path, name), format="ogg")
-       print("{0}/{1}.ogg created.".format(ogg_path, name))
-    else:
-        print("Coudln't create: " + name, ext)
+print("Beginning conversion of .mp3 to .ogg:")
 
-print("Finished mp3 -> ogg conversion.")
+for filename in tqdm(os.listdir(mp3_path)):
+    name, ext = os.path.splitext(filename)
+    if ext == '.mp3' and os.path.exists(filename) == False:
+       mp3 = AudioSegment.from_mp3(mp3_path + '\\' + filename)
+       mp3[start_time:end_time].export('{0}/{1}.ogg'.format(ogg_path, name), format='ogg')
+
+print('Finished .mp3 -> .ogg conversion.')
--- audio/wav-convert-trim.py
+++ audio/wav-convert-trim.py
@@ -1,6 +1,7 @@
 from pydub import AudioSegment
 import os
 import pandas as pd
+from tqdm import tqdm
 
 mp3_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'mp3'))
 wav_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'wav'))
@@ -8,13 +9,12 @@ wav_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'wav'))
 start_time = 0 # 0 sec
 end_time = 30000 # 30 sec
 
-for filename in os.listdir(mp3_path):
+print("Beginning conversion of .mp3 to .wav:")
+
+for filename in tqdm(os.listdir(mp3_path)):
     name, ext = os.path.splitext(filename)
-    if ext == ".mp3":
+    if ext == ".mp3" and os.path.exists(filename) == False:
        mp3 = AudioSegment.from_mp3(mp3_path + "\\" + filename)
        mp3[start_time:end_time].export("{0}/{1}.wav".format(wav_path, name), format="wav")
-       print("{0}/{1}.wav created.".format(wav_path, name))
-    else:
-        print(name, ext)
 
-print("Finished mp3 -> wav conversion.")
+print("Finished .mp3 -> .wav conversion.")
