db790ec929f6f6facd7ff469d7456e3f024b2d10
Sixten <landysixten@gmail.com>
2022-03-11 13:00:47 +0100

    Improve user interface and runtime

--- images/audio-to-mel.py
+++ images/audio-to-mel.py
@@ -3,29 +3,38 @@ import librosa
 import librosa.display
 import numpy as np
 import os
-from scipy.io.wavfile import read, write
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 wav_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'audio', 'wav'))
+ogg_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'audio', 'ogg'))
 df = pd.read_csv(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'audio', 'metadata.csv')))
 
-for i, path in enumerate(os.listdir(wav_path)):
-    audio_file = '{0}-{1}-{2}.wav'.format(df.Genus[i], df.Specific_epithet[i], df.Recording_ID[i])
-    output = "images/mel/{0}/{1}.png".format(df.English_name[i], df.Recording_ID[i])
+ogg_or_wav = input('Proceed with .ogg or .wav? (Enter "ogg" or "wav"): ')
+print('.{} selected.'.format(ogg_or_wav))
 
+if ogg_or_wav == 'ogg':
+    file_path = ogg_path
+    audio_file = '{0}-{1}-{2}.ogg'
+
+elif ogg_or_wav == 'wav':
+    file_path = wav_path
+    audio_file = '{0}-{1}-{2}.wav'
+
+else:
+    print('Unknown input.')
+
+for i, path in tqdm(enumerate(os.listdir(file_path))):
+    target = '{0}/{1}'.format(file_path, audio_file.format(df.Genus[i], df.Specific_epithet[i], df.Recording_ID[i])) 
+    output = 'images/mel/{0}/{1}.png'.format(df.English_name[i], df.Recording_ID[i])
     try:
-        y, sr = librosa.load(wav_path + "\\" + audio_file)
+        y = librosa.load(target)
         if os.path.exists(output):
-            print("{} already exists. Continuing.".format(output))
             continue
-        else:
-            pass
-
     except FileNotFoundError:
-        print("{} not found. Continuing.".format(audio_file))
         continue
 
-    mel_s = librosa.feature.melspectrogram(y, sr)
+    mel_s = librosa.feature.melspectrogram(y)
     mel_spectrogram = librosa.power_to_db(mel_s, ref=np.max)
     mel_img = librosa.display.specshow(mel_spectrogram)
 
@@ -33,10 +42,9 @@ for i, path in enumerate(os.listdir(wav_path)):
     try:
         plt.savefig(output)
     except FileNotFoundError:
-        print("Creating {}".format(output))
         os.makedirs(output)
         plt.savefig(output)
-
-    print("Conversion of {} successful.".format(output))
-
+        
     del y, sr, mel_s, mel_spectrogram, mel_img
+
+print('.{} to Mel-Spectrogram conversion completed.'.format(ogg_or_wav))
