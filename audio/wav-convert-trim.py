f0edd4f6322a995bcbea23f861bc43e8b8f0e96d
Sixten <landysixten@gmail.com>
2022-03-11 14:19:50 +0100

    Improve runtime

--- audio/wav-convert-trim.py
+++ audio/wav-convert-trim.py
@@ -13,8 +13,10 @@ print("Beginning conversion of .mp3 to .wav:")
 
 for filename in tqdm(os.listdir(mp3_path)):
     name, ext = os.path.splitext(filename)
-    if ext == ".mp3" and os.path.exists(filename) == False:
-       mp3 = AudioSegment.from_mp3(mp3_path + "\\" + filename)
-       mp3[start_time:end_time].export("{0}/{1}.wav".format(wav_path, name), format="wav")
+    if os.path.exists('{0}/{1}.wav'.format(wav_path, name)):
+        continue
+    if ext == '.mp3':
+       mp3 = AudioSegment.from_mp3(mp3_path + '\\' + filename)
+       mp3[start_time:end_time].export('{0}/{1}.wav'.format(wav_path, name), format='wav')
 
 print("Finished .mp3 -> .wav conversion.")
