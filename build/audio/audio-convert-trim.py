d9ce5e7df7863616dddfc1aae09dcf7048ab7728
sixtenl <91183041+sixtenl@users.noreply.github.com>
2022-03-15 23:03:09 +0100

    Update audio-convert-trim.py

--- build/audio/audio-convert-trim.py
+++ build/audio/audio-convert-trim.py
@@ -3,24 +3,44 @@ import os
 import pandas as pd
 from tqdm import tqdm
 
-format = input('Please enter the format you would like to convert the mp3 files to: (wav, ogg, aac, wma) ')
+print('This process will assist in converting and/or trimming every audio file in the dataset.')
+print('Please make sure the dataset exists in: "build/audio/mp3"')
+
+format = input('Input the desired format to convert the mp3 files to: (e.g. mp3, wav, ogg, aac, wma) ')
+time = int(input('Input the desired length in seconds of the audio file: (Enter 0 to keep the original length) ')) * 1000
+
 mp3_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'mp3'))
-path = os.path.realpath(os.path.join(os.path.dirname(__file__), format))
+if format == 'mp3':
+    path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'new_mp3'))
+else:
+    path = os.path.realpath(os.path.join(os.path.dirname(__file__), format))
 
-end_time = int(input('Please enter the desired length of the audio file: (Enter 0 to keep the original length) '))
-
-print("Beginning conversion of .mp3 to .ogg:")
+print('Beginning process...')
+print('Output format: {}'.format(format))
+if time == 0:
+    print('Length: unchanged')
+else:
+    print("Length: {} seconds".format(time / 1000))
 
 for filename in tqdm(os.listdir(mp3_path)):
     name, ext = os.path.splitext(filename)
     if not os.path.exists(path):
-        os.mkdir(format)
+        if format == 'mp3':
+            try:
+                os.mkdir(os.path.realpath(os.path.join(os.path.dirname(__file__))) + '/' + 'new_mp3')
+            except FileExistsError:
+                pass
+        else:
+            try:
+                os.mkdir(os.path.realpath(os.path.join(os.path.dirname(__file__))) + '/' + format)
+            except FileExistsError:
+                pass
     if os.path.exists('{0}/{1}.{2}'.format(path, name, format)):
         continue
     if ext == '.mp3':
         mp3 = AudioSegment.from_mp3(mp3_path + '\\' + filename)
-        if end_time > 0:
-           mp3[0:end_time].export('{0}/{1}'.format(path, name), format=format)
+        if time > 0:
+           mp3[0:time].export('{0}/{1}.{2}'.format(path, name, format), format=format)
         else:
             mp3.export('{0}/{1}.{2}'.format(path, name, format), format=format)
 
