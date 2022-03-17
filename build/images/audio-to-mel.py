ab6b2733434250c60b4ee5a6134a46e01d5fd954
sixtenl <91183041+sixtenl@users.noreply.github.com>
2022-03-17 19:26:18 +0100

    Update audio-to-mel.py

--- build/images/audio-to-mel.py
+++ build/images/audio-to-mel.py
@@ -13,7 +13,7 @@ audio_format = input('Specify the desired AUDIO format: ')
 if audio_format == 'mp3' and os.path.exists(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'audio', 'new_mp3'))):
     mp3_check = input('Do you want to use the modified mp3 files in "audio/new_mp3"? (y/n): ')
 audio_format = audio_format.lower()
-image_format = input('Specify the desired IMAGE format (eps, jpeg, jpg, pdf, pgf, png, ps, raw, rgba, svg, svgz, tif, tiff): ')
+image_format = input('Specify the desired IMAGE format (jpeg, jpg, png, raw, tif, tiff): ')
 image_format = image_format.lower()
 
 if mp3_check == 'y':
@@ -24,15 +24,17 @@ else:
 print('{0} -> {1} selected.'.format(audio_format, image_format))
 print('Converting every file in {}'.format(path))
 
+row_list = []
 for i in tqdm(range(len(df))):
     audio_file = '{0}-{1}-{2}.{3}'.format(df.Genus[i], df.Specific_epithet[i], df.Recording_ID[i], audio_format)
     target = '{0}\\{1}'.format(path, audio_file)
-    output = '{0}\\{1}\\{2}.png'.format(mel_path, df.English_name[i], df.Recording_ID[i])
+    output = '{0}\\{1}.{2}'.format(mel_path, df.Recording_ID[i], image_format)
     if os.path.exists(output):
+        row_list.append(dict(file_name='{0}.{1}'.format(df.Recording_ID[i], image_format), english_name=df.English_name[i]))
         continue
     try:
         y, sr = librosa.load(target)
-    except FileNotFoundError:
+    except:
         continue
 
     mel_s = librosa.feature.melspectrogram(y, sr)
@@ -40,12 +42,13 @@ for i in tqdm(range(len(df))):
     mel_img = librosa.display.specshow(mel_spectrogram)
 
     plt.tight_layout()
-    try:
-        plt.savefig(output, format=image_format)
-    except FileNotFoundError:
-        os.makedirs('{0}\\{1}'.format(mel_path, df.English_name[i]))
-        plt.savefig(output, format=image_format)
-    
+    plt.savefig(output, format=image_format)
+
+    row_list.append(dict(file_name='{0}.{1}'.format(df.Recording_ID[i], image_format), english_name=df.English_name[i]))
+
     del y, sr, mel_s, mel_spectrogram, mel_img
+
+new_df = pd.DataFrame(row_list, columns=['file_name', 'english_name'])
+new_df.to_csv('{}\\train.csv'.format(mel_path), index=False)
 
 print('.{0} -> {1} conversion completed.'.format(audio_format, image_format))
