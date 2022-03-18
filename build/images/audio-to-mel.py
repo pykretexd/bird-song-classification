50e41016f991f0116f446654b2ceddfe595cc2c1
sixtenl <91183041+sixtenl@users.noreply.github.com>
2022-03-18 12:46:16 +0100

    Update audio-to-mel.py

--- build/images/audio-to-mel.py
+++ build/images/audio-to-mel.py
@@ -3,8 +3,10 @@ import librosa
 import librosa.display
 import numpy as np
 import os
-import matplotlib.pyplot as plt
+import skimage.io
 from tqdm import tqdm
+import warnings
+warnings.filterwarnings("ignore")
 
 mel_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'mel'))
 df = pd.read_csv(os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'audio', 'metadata.csv')))
@@ -24,8 +26,13 @@ else:
 print('{0} -> {1} selected.'.format(audio_format, image_format))
 print('Converting every file in {}'.format(path))
 
+def scale_minmax(X, min=0.0, max=1.0):
+    X_std = (X - X.min()) / (X.max() - X.min())
+    X_scaled = X_std * (max - min) + min
+    return X_scaled
+
 row_list = []
-for i in tqdm(range(len(df))):
+for i in tqdm(range(250)):
     audio_file = '{0}-{1}-{2}.{3}'.format(df.Genus[i], df.Specific_epithet[i], df.Recording_ID[i], audio_format)
     target = '{0}\\{1}'.format(path, audio_file)
     output = '{0}\\{1}.{2}'.format(mel_path, df.Recording_ID[i], image_format)
@@ -37,16 +44,15 @@ for i in tqdm(range(len(df))):
     except:
         continue
 
-    mel_s = librosa.feature.melspectrogram(y, sr)
-    mel_spectrogram = librosa.power_to_db(mel_s, ref=np.max)
-    mel_img = librosa.display.specshow(mel_spectrogram)
-
-    plt.tight_layout()
-    plt.savefig(output, format=image_format)
+    mel_s = librosa.feature.melspectrogram(y=y, sr=sr)
+    mel_s = np.log(mel_s + 1e-9)
+    mel_img = librosa.display.specshow(mel_s)
+    mel_img = scale_minmax(mel_s, 0, 255).astype(np.uint8)
+    skimage.io.imsave(output, mel_img)
 
     row_list.append(dict(file_name='{0}.{1}'.format(df.Recording_ID[i], image_format), english_name=df.English_name[i]))
 
-    del y, sr, mel_s, mel_spectrogram, mel_img
+    del y, sr, mel_s, mel_img
 
 new_df = pd.DataFrame(row_list, columns=['file_name', 'english_name'])
 new_df.to_csv('{}\\train.csv'.format(mel_path), index=False)
