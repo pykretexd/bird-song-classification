afcb93358f14d0d0328cc35c735522986e2683db
sixtenl <91183041+sixtenl@users.noreply.github.com>
2022-03-18 16:25:37 +0100

    Update audio-to-mel.py

--- build/images/audio-to-mel.py
+++ build/images/audio-to-mel.py
@@ -3,7 +3,7 @@ import librosa
 import librosa.display
 import numpy as np
 import os
-import skimage.io
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 import warnings
 warnings.filterwarnings("ignore")
@@ -26,31 +26,23 @@ else:
 print('{0} -> {1} selected.'.format(audio_format, image_format))
 print('Converting every file in {}'.format(path))
 
-def scale_minmax(X, min=0.0, max=1.0):
-    X_std = (X - X.min()) / (X.max() - X.min())
-    X_scaled = X_std * (max - min) + min
-    return X_scaled
-
 row_list = []
-for i in tqdm(range(250)):
+for i in tqdm(range(100)):
+    row_list.append(dict(file_name='{0}.{1}'.format(df.Recording_ID[i], image_format), english_name=df.English_name[i]))
     audio_file = '{0}-{1}-{2}.{3}'.format(df.Genus[i], df.Specific_epithet[i], df.Recording_ID[i], audio_format)
     target = '{0}\\{1}'.format(path, audio_file)
     output = '{0}\\{1}.{2}'.format(mel_path, df.Recording_ID[i], image_format)
     if os.path.exists(output):
-        row_list.append(dict(file_name='{0}.{1}'.format(df.Recording_ID[i], image_format), english_name=df.English_name[i]))
         continue
     try:
         y, sr = librosa.load(target)
     except:
         continue
 
-    mel_s = librosa.feature.melspectrogram(y=y, sr=sr)
-    mel_s = np.log(mel_s + 1e-9)
+    mel_s = librosa.power_to_db(librosa.feature.melspectrogram(y=y, sr=sr), ref=np.max)
     mel_img = librosa.display.specshow(mel_s)
-    mel_img = scale_minmax(mel_s, 0, 255).astype(np.uint8)
-    skimage.io.imsave(output, mel_img)
-
-    row_list.append(dict(file_name='{0}.{1}'.format(df.Recording_ID[i], image_format), english_name=df.English_name[i]))
+    plt.tight_layout()
+    plt.savefig(output, format=image_format, dpi=50)
 
     del y, sr, mel_s, mel_img
 
