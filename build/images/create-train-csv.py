531f88c2624d1267193f3ab053ec80bde25bc3e3
sixtenl <91183041+sixtenl@users.noreply.github.com>
2022-03-21 11:54:59 +0100

    Delete create-train-csv.py

--- build/images/create-train-csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-dir = os.path.realpath(os.path.join(os.path.dirname(__file__), 'metadata.csv'))
-export_destination = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'images', 'mel'))
-df = pd.read_csv(dir, usecols=[0, 3])
-df.to_csv("{}\\train.csv".format(export_destination), index=False)
