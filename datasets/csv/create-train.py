14ec6957bdb54c845212066bb3fb5bdbc5113fb7
sixtenl <91183041+sixtenl@users.noreply.github.com>
2022-03-21 10:54:17 +0100

    Update and rename datasets/create-train.py to datasets/csv/create-train.py

--- datasets/csv/create-train.py
+++ datasets/csv/create-train.py
@@ -1,11 +1,12 @@
-from email.utils import decode_rfc2231
 import pandas as pd
 import os
+
+from pyparsing import col
 
 def filter_rows_by_values(df, col, values):
     return df[~df[col].isin(values)]
 
-directory = 'datasets/'
+directory = 'datasets/csv/'
 
 df1 = pd.read_csv(directory + 'xeno-canto_europe.csv', usecols=[1, 4, 6])
 df1['file_code'] = 'XC' + df1['file_code'].astype(str) + '.mp3'
@@ -14,7 +15,11 @@ df1 = df1.rename(columns={'file_code': 'filename'})
 df2 = pd.read_csv(directory + 'train_extended.csv', usecols=[6, 7, 22])
 df2 = df2.rename(columns={'species': 'english_name'})
 
-df = pd.concat([df1, df2], ignore_index=True)
+df3 = pd.read_csv(directory + 'metadata.csv', usecols=[0, 4, 6])
+df3 = df3.rename(columns={'Recording_ID': 'filename', 'English_name': 'english_name', 'Country': 'country'})
+df3 = df3['Recording_ID'] = 'XC' + df3['Recording_ID'].astype(str) + '.mp3'
+
+df = pd.concat([df1, df2, df3], ignore_index=True)
 df = filter_rows_by_values(df, 'country', ['Japan', 'El Salvador', 'Guatemala', 'Chile', 'Peru', 'United States', 'Canada', 'Colombia', 'Costa Rica', 'Mexico', 'Honduras', 'Puerto Rico', 'Cuba', 'Panama', 'Ecuador', 'Algeria', 'Argentina', 'Australia', 'Azerbaijan', 'Bahamas', 'Barbados', 'Belize', 'Bhutan', 'Bolivia', 'Brazil', 'Burundi', 'Vietnam', 'Venezuela', 'Uzbekistan', 'Uruguay'])
 
 print(df.head())
