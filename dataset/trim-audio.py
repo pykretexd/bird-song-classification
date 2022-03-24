ef85fad04ca8e78433fd8fbb27daf1e7e0417632
sixtenl <91183041+sixtenl@users.noreply.github.com>
2022-03-24 09:40:16 +0100

    Update trim-audio.py

--- dataset/trim-audio.py
+++ dataset/trim-audio.py
@@ -1,3 +1,4 @@
+from numpy import number
 import pandas as pd
 import os
 from pydub import AudioSegment
@@ -13,13 +14,43 @@ for folder in next(os.walk(path, '.'))[1]:
     folder_path = path + '\\' + folder
     for file in tqdm(os.listdir(folder_path)):
         file_path = '{}\\{}'.format(folder_path, file)
-        export_file = '{}\\{}'.format(path, file)
-        list.append(dict(file_name=file, label=folder))
-        if os.path.exists(export_file):
-            # os.remove(file_path)
+        if os.path.exists('{}\\1-{}'.format(path, file)):
+            list.append(dict(file_name='1-{}'.format(file), label=folder))
             continue
+        elif os.path.exists('{}\\2-{}'.format(path, file)):
+            list.append(dict(file_name='2-{}'.format(file), label=folder))
+            continue
+        elif os.path.exists('{}\\3-{}'.format(path, file)):
+            list.append(dict(file_name='3-{}'.format(file), label=folder))
+            continue
+        elif os.path.exists('{}\\4-{}'.format(path, file)):
+            list.append(dict(file_name='4-{}'.format(file), label=folder))
+            continue
+        elif os.path.exists('{}\\5-{}'.format(path, file)):
+            list.append(dict(file_name='5-{}'.format(file), label=folder))
+            continue
+        elif os.path.exists('{}\\6-{}'.format(path, file)):
+            list.append(dict(file_name='6-{}'.format(file), label=folder))
+            continue
+        elif os.path.exists('{}\\7-{}'.format(path, file)):
+            list.append(dict(file_name='7-{}'.format(file), label=folder))
+            continue
+        elif os.path.exists('{}\\8-{}'.format(path, file)):
+            list.append(dict(file_name='8-{}'.format(file), label=folder))
+            continue
+        
+        
         mp3 = AudioSegment.from_mp3(file_path)
-        mp3[0:time].export(export_file, format='mp3')
+        number_of_exports = round(mp3.duration_seconds / time)
+        print(number_of_exports)
+        
+        i = 1
+        while i <= number_of_exports:
+            end = time * i
+            start = end - time
+            mp3[start:end].export('{}\\{}-{}'.format(path, i, file), format='mp3')
+            list.append(dict(file_name='{}-{}'.format(i, file), label=folder))
+            i += 1
 
 df = pd.DataFrame(list)
 df.to_csv(path + '\\' + 'audio.csv', index=False)
