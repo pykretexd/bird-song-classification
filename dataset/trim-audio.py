2172267aca3cb5d6a56489c0c8f931042719f13e
sixtenl <91183041+sixtenl@users.noreply.github.com>
2022-03-24 11:53:02 +0100

    Update trim-audio.py

--- dataset/trim-audio.py
+++ dataset/trim-audio.py
@@ -14,38 +14,18 @@ for folder in next(os.walk(path, '.'))[1]:
     folder_path = path + '\\' + folder
     for file in tqdm(os.listdir(folder_path)):
         file_path = '{}\\{}'.format(folder_path, file)
-        if os.path.exists('{}\\1-{}'.format(path, file)):
+        if os.path.exists('{}\\{}'.format(path, file)):
+            os.rename('{}\\{}'.format(path, file), '{}\\1-{}'.format(path, file))
             list.append(dict(file_name='1-{}'.format(file), label=folder))
-            continue
-        elif os.path.exists('{}\\2-{}'.format(path, file)):
-            list.append(dict(file_name='2-{}'.format(file), label=folder))
-            continue
-        elif os.path.exists('{}\\3-{}'.format(path, file)):
-            list.append(dict(file_name='3-{}'.format(file), label=folder))
-            continue
-        elif os.path.exists('{}\\4-{}'.format(path, file)):
-            list.append(dict(file_name='4-{}'.format(file), label=folder))
-            continue
-        elif os.path.exists('{}\\5-{}'.format(path, file)):
-            list.append(dict(file_name='5-{}'.format(file), label=folder))
-            continue
-        elif os.path.exists('{}\\6-{}'.format(path, file)):
-            list.append(dict(file_name='6-{}'.format(file), label=folder))
-            continue
-        elif os.path.exists('{}\\7-{}'.format(path, file)):
-            list.append(dict(file_name='7-{}'.format(file), label=folder))
-            continue
-        elif os.path.exists('{}\\8-{}'.format(path, file)):
-            list.append(dict(file_name='8-{}'.format(file), label=folder))
-            continue
-        
         
         mp3 = AudioSegment.from_mp3(file_path)
-        number_of_exports = round(mp3.duration_seconds / time)
-        print(number_of_exports)
-        
+        number_of_exports = round(mp3.duration_seconds) // (time // 1000)
         i = 1
         while i <= number_of_exports:
+            if os.path.exists('{}\\{}-{}'.format(path, i, file)):
+                list.append(dict(file_name='{}-{}'.format(i, file), label=folder))
+                i += 1
+                continue
             end = time * i
             start = end - time
             mp3[start:end].export('{}\\{}-{}'.format(path, i, file), format='mp3')
